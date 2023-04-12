d72a6a40a2d42f80af8c153ae35339dcfae7f40e
kristaponis <a.kristaponis@icloud.com>
2023-04-12 09:23:18 +0300

    Created and rendered first template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello Flask"
+    return render_template("home.html")
 
 
 app.run(port=8000)
