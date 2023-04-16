7cd48d95f47a63f1cb85d3537705431bf1aa74c4
kristaponis <a.kristaponis@icloud.com>
2023-04-16 19:13:56 +0300

    Added market page with some sample data

--- app.py
+++ app.py
@@ -10,4 +10,15 @@ def hello():
     return render_template("home.html")
 
 
+@app.route("/market")
+def market():
+    items = [
+        {"id": 1, "name": "Apple", "price": 2000},
+        {"id": 2, "name": "Dell", "price": 1300},
+        {"id": 3, "name": "Microsoft", "price": 1000},
+    ]
+
+    return render_template("market.html", items=items)
+
+
 app.run(port=8000)
